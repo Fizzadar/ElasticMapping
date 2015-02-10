78b6312c9de14080d313a4010707bb32d2cfdfe1
Nick Barrett <nick@oxygem.com>
2015-02-10 17:06:05 +0000

    v1.0, tidy up

--- setup.py
+++ setup.py
@@ -9,14 +9,16 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    version = '0.0',
-    name = 'ElasticMapping',
-    description = 'A simple mappings builder for Elasticsearch',
-    author = 'Nick Barrett',
-    author_email = 'pointlessrambler@gmail.com',
-    url = 'http://github.com/Fizzadar/ElasticMapping',
-    package_dir= {'ElasticMapping': 'elasticmapping'},
-    packages = [
+    version='1.0',
+    name='ElasticMapping',
+    description='A simple mappings builder for Elasticsearch',
+    author='Nick Barrett',
+    author_email='pointlessrambler@gmail.com',
+    url='http://github.com/Fizzadar/ElasticMapping',
+    package_dir={
+        'ElasticMapping': 'elasticmapping'
+    },
+    packages=[
         'elasticmapping'
     ]
 )
